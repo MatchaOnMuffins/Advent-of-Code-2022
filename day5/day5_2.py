1170d33a0cf59011909a6155fa445820c876f685
MatchaOnMuffins <103859142+MatchaOnMuffins@users.noreply.github.com>
2022-12-05 14:40:24 -0500

    Day 5

---
+++ day5/day5_2.py
@@ -0,0 +1,42 @@
+import re
+
+"""
+                        [R] [J] [W]
+            [R] [N]     [T] [T] [C]
+[R]         [P] [G]     [J] [P] [T]
+[Q]     [C] [M] [V]     [F] [F] [H]
+[G] [P] [M] [S] [Z]     [Z] [C] [Q]
+[P] [C] [P] [Q] [J] [J] [P] [H] [Z]
+[C] [T] [H] [T] [H] [P] [G] [L] [V]
+[F] [W] [B] [L] [P] [D] [L] [N] [G]
+ 1   2   3   4   5   6   7   8   9 
+"""
+
+stacks = ['RQGPCF', 'PCTW', 'CMPHB', 'RPMSQTL', 'NGVZJHP', 'JPD', 'RTJFZPGL', 'JTPFCHLN', 'WCTHQZVG']
+stacks = [list(s) for s in stacks]
+for stack in stacks:
+    stack.reverse()
+
+reExp = re.compile(r'^move (\d+) from (\d) to (\d)')
+
+with open('day5.in', 'r') as f:
+    for i in range(10):
+        f.readline()
+    for i in range(502):
+        line = f.readline()
+        m = reExp.match(line)
+        containerCount = int(m.group(1))
+        fromStack = int(m.group(2))
+        toStack = int(m.group(3))
+        # print('Move {} containers from stack {} to stack {}'.format(containerCount, fromStack, toStack))
+        temp = []
+        for j in range(containerCount):
+            temp.append(stacks[fromStack - 1].pop())
+        temp.reverse()
+        for tmp in temp:
+            stacks[toStack - 1].append(tmp)
+
+ans = ''
+for i in range(9):
+    ans += stacks[i][-1]
+print('Answer:', ans)
